55217def61a9089f6bc9df749c657e4f41edd672
Pawel Psztyc <jarrodek@gmail.com>
2018-04-12 17:21:18 -0700

    Adding tests

---
+++ test/amf-loader.js
@@ -0,0 +1,28 @@
+const AmfLoader = {};
+AmfLoader.load = function(endpointIndex, methodIndex) {
+  endpointIndex = endpointIndex || 0;
+  methodIndex = methodIndex || 0;
+  const url = location.protocol + '//' + location.host +
+    location.pathname.substr(0, location.pathname.lastIndexOf('/'))
+    .replace('/test', '/demo') + '/demo-api.json';
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', (e) => {
+      let data;
+      try {
+        data = JSON.parse(e.target.response);
+      } catch (e) {
+        reject(e);
+        return;
+      }
+      const def = data[0]['http://raml.org/vocabularies/document#encodes'][0];
+      const endpoint = def['http://raml.org/vocabularies/http#endpoint'][endpointIndex];
+      const method = endpoint['http://www.w3.org/ns/hydra/core#supportedOperation'][methodIndex];
+      resolve([data, endpoint, method]);
+    });
+    xhr.addEventListener('error',
+      () => reject(new Error('Unable to load model file')));
+    xhr.open('GET', url);
+    xhr.send();
+  });
+};
